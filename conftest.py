86ff5a822076add5c427cde2b69a0107f4e27d60
Gabriel Lima <glima.smoreira@gmail.com>
2026-09-14 16:20:21 -0300

    Configura o Chrome para reduzir avisos durante os testes

--- conftest.py
+++ conftest.py
@@ -22,6 +22,14 @@ def driver():
     options = webdriver.ChromeOptions()
     options.add_argument("--start-maximized")
 
+    options.add_argument("--disable-save-password-bubble")
+    options.add_argument("--disable-notifications")
+    options.add_experimental_option("prefs", {
+    "credentials_enable_service": False,
+    "profile.password_manager_enabled": False,
+    "profile.password_manager_leak_detection": False
+    })
+
     d = webdriver.Chrome(options=options)
 
     yield d
